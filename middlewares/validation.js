1b1e0b5181414cd57917f6c1f519094a57a33cc8
Kirill Dvoryaninov <kdvoryaninov@yandex.ru>
2023-06-12 19:18:59 +0300

    a

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -16,14 +16,14 @@ const validateCreateUser = celebrate({
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().custom(validateUrl),
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(10),
+    password: Joi.string().required().min(8),
   }),
 });
 
 const validateLogin = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(10),
+    password: Joi.string().required().min(8),
   }),
 });
 
