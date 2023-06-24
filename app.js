e1b0d9a13be5f64236ac36f559fe2a6d670f4786
Kirill Dvoryaninov <kdvoryaninov@yandex.ru>
2023-06-24 14:45:54 +0300

    Fix1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const rateLimit = require('express-rate-limit');
+// const rateLimit = require('express-rate-limit');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
@@ -27,12 +27,12 @@ mongoose.connect(MONGO_URL, {
 
 app.use(cookieParser());
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  standardHeaders: true,
-  legacyHeaders: false,
-});
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: 100,
+//   standardHeaders: true,
+//   legacyHeaders: false,
+// });
 
 app.use(requestLogger);
 app.post('/signin', validateLogin, login);
@@ -41,7 +41,7 @@ app.post('/signup', validateCreateUser, createUser);
 app.use(auth);
 app.use(routes);
 
-app.use(limiter);
+// app.use(limiter);
 app.use(helmet());
 
 app.use(errorLogger);
