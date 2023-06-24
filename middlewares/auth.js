006a7c7d9d610c54d4e5c4e15d2ad36a9b3225c8
Kirill Dvoryaninov <kdvoryaninov@yandex.ru>
2023-06-24 19:27:56 +0300

    Fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,6 @@ const UnauthorizedError = require('../errors/unauthorized_error');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  // извлечём токен
   const token = req.cookies.jwt;
   // верифицируем токен
   let payload;
